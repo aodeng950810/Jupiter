d22fb83af1ee3baf5c03f5b45c12ad0d6985ea1d
aodeng950810 <ad4731@nyu.edu>
2020-03-02 22:08:06 -0500

    First commit

---
+++ src/com/laioffer/event1/Searchitem.java
@@ -0,0 +1,87 @@
+package com.laioffer.event1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.MySQLConnection;
+import entity.Item;
+import external.TicketMasterClient;
+
+/**
+ * Servlet implementation class Searchitem
+ */
+@WebServlet("/search")
+public class Searchitem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Searchitem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+
+		// optional
+		String userId = session.getAttribute("user_id").toString(); 
+
+		//String userId = request.getParameter("user_id");
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		String term = request.getParameter("term");
+		//use the written Ticketmaster client to call ticketmaster API to get data
+
+		TicketMasterClient client = new TicketMasterClient();
+		
+		List<Item> items = client.search(lat, lon, null);
+		MySQLConnection connection = new MySQLConnection();
+		Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+		connection.close();
+
+		JSONArray array = new JSONArray();
+		for(Item item: items) {
+			JSONObject obj = item.toJSONObject();
+			try {
+				obj.put("favorite", favoritedItemIds.contains(item.getItemId()));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			array.put(obj);
+		}
+		
+		RpcHelper.writeJsonArray(response,array);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
